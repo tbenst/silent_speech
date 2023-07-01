2f70ef78785577a02a17221b691fa00c122a67b8
Tyler Benster <gaia@tylerbenster.com>
2023-07-01 16:48:25 -0700

    try to run contrastive loss on CPU

--- notebooks/tyler/2023-06-22_librispeech_contrastive.py
+++ notebooks/tyler/2023-06-22_librispeech_contrastive.py
@@ -547,7 +547,7 @@ class SpeechOrEMGToText(Model):
             # frame-aligned phoneme labels for those
             z = torch.concatenate([paired_e_z, *audio_z]).to('cpu')
             z_class = torch.concatenate([*paired_e_phonemes, *audio_phonemes]).to('cpu')
-            sup_nce_loss = supervised_contrastive_loss(z, z_class, 'cpu')
+            sup_nce_loss = supervised_contrastive_loss(z, z_class, device='cpu')
                 # device=self.device)
         elif emg_z is not None:
             # INFO: phoneme labels aren't frame-aligned with emg, so we can't use them
