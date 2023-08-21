7322cb6f971cfd34a009a96af1354744fdaebd2e
Tyler Benster <github@tylerbenster.com>
2023-08-21 13:32:29 -0700

    fix

--- architecture.py
+++ architecture.py
@@ -260,9 +260,12 @@ class Model(pl.LightningModule):
         # but this seems forced by pytorch lightning
         # maybe should use Fabric in the future..
         if self.trainer.datamodule is not None:
-            if hasattr(self.trainer.datamodule, 'TrainBatchSampler'):
-                logging.debug(f"set epoch to {self.current_epoch=}")
+            try:
                 self.trainer.datamodule.TrainBatchSampler.set_epoch(self.current_epoch)
+                logging.debug(f"set epoch to {self.current_epoch=}")
+            except:
+                # not all datamodules have a TrainBatchSampler, or a set_epoch method
+                pass
     
     def training_step(self, batch, batch_idx):
         loss, bz = self.calc_loss(batch)
