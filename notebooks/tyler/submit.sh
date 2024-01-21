d11fd3b0e75eaa1326f16ddb3855ae52039e841d
Tyler Benster <github@tylerbenster.com>
2024-01-20 18:45:28 -0800

    update slurm script

--- notebooks/tyler/submit.sh
+++ notebooks/tyler/submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -p normal,owners,shauld,deissero
+#SBATCH -p henderj
 #SBATCH --job-name=interactive
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=120G
-#SBATCH --constraint=GPU_MEM:32GB
+#SBATCH --mem=60G
+#SBATCH --constraint=GPU_MEM:80GB
 
 . activate magneto
 cd /home/users/tbenst/code/silent_speech/notebooks/tyler
-python 2023-07-25_dtw_speech_silent_emg.py
+python 2024-01-15_icml_models.py --no-dtw
