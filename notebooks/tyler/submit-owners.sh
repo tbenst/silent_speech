b7857f1296345e514a311cdf6695bd25a186d215
Tyler Benster <github@tylerbenster.com>
2024-01-22 00:08:41 -0800

    submit owners

---
+++ notebooks/tyler/submit-owners.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p henderj,owners
+#SBATCH --job-name=interactive
+#SBATCH --time=48:00:00
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=60G
+#SBATCH --constraint=GPU_MEM:80GB
+
+. activate magneto
+cd /home/users/tbenst/code/silent_speech/notebooks/tyler
+python 2024-01-15_icml_models.py "$@"
